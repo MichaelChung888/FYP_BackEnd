a7f1e2a1c3b27f0da0e3963f2edfdda4bfa9b6f5
MichaelChung888 <michaelchung88888@gmail.com>
2024-01-27 12:49:12 +0000

    Simple API Server

---
+++ Blog.fs
@@ -0,0 +1,75 @@
+﻿module Blog 
+
+open System
+open System.IO
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Http
+open Giraffe
+
+[<CLIMutable>]
+//--------------------------------------------------------------------------------------//
+//                                      Main code                                       //
+//--------------------------------------------------------------------------------------//
+
+type LoginInfo = {
+    username: string
+    password: string
+}
+
+let username = "Michael"
+let password = "Chung"
+
+let loginHttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! data = ctx.BindJsonAsync<LoginInfo>()
+            if data.username = "Michael" && data.password = "Chung" 
+            then
+                ctx.SetStatusCode 200
+                return! (json ("Logged In") ) next ctx
+            else
+                ctx.SetStatusCode 400
+                return! json ("Incorrect Username or Password") next ctx // Task.FromResult None
+        }
+
+//--------------------------------------------------------------------------------------//
+//                            Boilerplate code (IREELEVANT)                             //
+//--------------------------------------------------------------------------------------//
+
+(*
+
+type BlogPost = {
+    title: string
+    content: string
+}
+
+type BlogDb() =
+
+    let mutable allBlogPosts : BlogPost list = []
+
+    member this.GetAllPosts = fun() -> allBlogPosts 
+
+    member this.AddPost (newPost : BlogPost) =
+        allBlogPosts <- (newPost::allBlogPosts)
+        allBlogPosts
+
+type BlogServiceTree = {
+    getBlogDb : unit -> BlogDb
+}
+
+let getPostsHttpHandler (serviceTree: BlogServiceTree) =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        printfn "HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
+        json (serviceTree.getBlogDb().GetAllPosts()) next ctx
+
+let createPostHttpHandler (serviceTree: BlogServiceTree) =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! newPostJson = ctx.BindJsonAsync<BlogPost>()
+            serviceTree.getBlogDb().AddPost(newPostJson) |> ignore
+            return! json (newPostJson) next ctx
+        }
+        // Giraffe uses the native .NET "Task" type which are objects that replace the historic F# async {} workflows.
+        // It is useful as it removes the necessity of converting between tasks and async workflows (because ASP.NET Core
+        // can only work with tasks)
+*)
---
+++ Program.fs
@@ -0,0 +1,92 @@
+﻿open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Cors
+
+open Blog
+open Giraffe
+
+(* Web App Configuration *)
+
+let webApp = 
+    (*
+    let blogDb = new BlogDb()
+
+    let serviceTree = {
+        getBlogDb = fun() -> blogDb
+    }
+    *)
+
+
+    choose [
+        route "/" >=> text "Server Online"
+        subRoute "/login"
+            (choose [
+                route "" >=> warbler (fun _ -> loginHttpHandler)
+            ])
+        (*
+        subRoute "/posts" 
+            (choose [
+                route "" >=> GET >=> warbler (fun _ -> (getPostsHttpHandler serviceTree))
+                route "/create" >=> POST >=> warbler (fun _ -> (createPostHttpHandler serviceTree))
+            ])
+
+        *)
+
+
+    ]
+    // Note: warbler is used when the route is returning a dynamic (not static) response, hence wrap the function in a "warbler()"
+    // This is because functions in f# are eagerly evaluated, hence a normal route will only be evaluated the first time.
+    // warbler will ensure that the function is evaluated every time a route is hit.
+
+(* Infrastructure Configuration *)
+
+// Define the error handler function
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse
+    >=> ServerErrors.INTERNAL_ERROR ex.Message
+
+// Register all ASP.NET Core middleware
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+    (match env.IsDevelopment() with
+    | true  ->
+        app.UseDeveloperExceptionPage()
+    | false ->
+        app .UseGiraffeErrorHandler(errorHandler)
+            // .UseAuthentication() // If you were doing authentication
+            .UseHttpsRedirection())
+
+        .UseCors(fun builder -> builder.WithOrigins("http://localhost:5173").AllowAnyMethod().AllowAnyHeader() |> ignore)
+        //.UseStaticFiles()
+        .UseGiraffe(webApp)
+    (*
+    app.UseGiraffeErrorHandler(errorHandler)
+       .UseGiraffe webApp
+    app.UseCors(fun builder -> builder.WithOrigins("http://localhost:1234").AllowAnyMethod().AllowAnyHeader() |> ignore)
+       |> ignore
+    *)
+
+
+let configureServices (services : IServiceCollection) =
+    services.AddCors()    |> ignore
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    // Calling Configure to set up all middleware
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    .UseUrls("http://localhost:1234")
+                    |> ignore)
+        .Build()
+        .Run()
+    0
